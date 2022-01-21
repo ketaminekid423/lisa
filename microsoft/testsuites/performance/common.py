b287b9c735c7b07efaf72853281c9e5c8373484a
Lili Deng <lildeng@microsoft.com>
2022-01-21 16:46:42 +0800

    Migrate ntttcp and iperf (tcp, udp and synthetic, sriov) test cases.

--- microsoft/testsuites/performance/common.py
+++ microsoft/testsuites/performance/common.py
@@ -3,10 +3,11 @@
 import pathlib
 from typing import Dict, List, Optional
 
-from lisa import Node, notifier
+from lisa import Logger, Node, notifier
 from lisa.environment import Environment
 from lisa.messages import DiskPerformanceMessage, DiskSetupType, DiskType
-from lisa.tools import FIOMODES, Fio, FIOResult, Kill
+from lisa.schema import NetworkDataPath
+from lisa.tools import FIOMODES, Fio, FIOResult, Kill, Sed, Sysctl
 from lisa.util import dict_to_fields
 
 
@@ -74,6 +75,50 @@ def handle_and_send_back_results(
         fio_message.disk_setup_type = disk_setup_type
         fio_message.disk_type = disk_type
         notifier.notify(fio_message)
+
+
+def get_nic_datapath(node: Node) -> str:
+    data_path: str = ""
+    assert (
+        node.capability.network_interface
+        and node.capability.network_interface.data_path
+    )
+    if isinstance(node.capability.network_interface.data_path, NetworkDataPath):
+        data_path = node.capability.network_interface.data_path.value
+    return data_path
+
+
+def restore_sysctl_setting(
+    nodes: List[Node], perf_tuning: Dict[str, List[Dict[str, str]]]
+) -> None:
+    for node in nodes:
+        sysctl = node.tools[Sysctl]
+        for variable_list in perf_tuning[node.name]:
+            # restore back to the original value after testing
+            for variable, value in variable_list.items():
+                sysctl.write(variable, value)
+
+
+def set_systemd_tasks_max(nodes: List[Node], log: Logger) -> None:
+    for node in nodes:
+        if node.shell.exists(
+            node.get_pure_path("/usr/lib/systemd/system/user-.slice.d/10-defaults.conf")
+        ):
+            node.tools[Sed].substitute(
+                regexp="TasksMax.*",
+                replacement="TasksMax=122880",
+                file="/usr/lib/systemd/system/user-.slice.d/10-defaults.conf",
+                sudo=True,
+            )
+        elif node.shell.exists(node.get_pure_path("/etc/systemd/logind.conf")):
+            node.tools[Sed].append(
+                "UserTasksMax=122880", "/etc/systemd/logind.conf", sudo=True
+            )
+        else:
+            log.debug(
+                "no config file exist for systemd, either there is no systemd"
+                " service or the config file location is incorrect."
+            )
 
 
 def cleanup_process(environment: Environment, process_name: str) -> None:
